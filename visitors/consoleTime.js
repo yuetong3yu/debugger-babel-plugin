c25058109848cecba9d0acb588524f57d87c955a
yuetong3yu <yuetong3yu@gmail.com>
2022-02-28 17:50:36 +0800

    visitor

--- visitors/consoleTime.js
+++ visitors/consoleTime.js
@@ -1,6 +1,35 @@
 export function consoleTime({ types: t }) {
   return {
     name: 'consoleTime',
-    visitor: {},
+    visitor: {
+      FunctionDeclaration: {
+        enter(path) {
+          const body = path.get('body')
+          body.unshiftContainer(
+            'body',
+            t.callExpression(
+              t.memberExpression(t.identifier('console'), t.identifier('time')),
+              [t.stringLiteral(path.node.id.name)]
+            )
+          )
+        },
+        exit(path) {
+          const blockStatment = path.get('body')
+          const lastExpression = blockStatment.get('body').pop()
+          const timeEndStatment = t.callExpression(
+            t.memberExpression(
+              t.identifier('console'),
+              t.identifier('timeEnd')
+            ),
+            [t.stringLiteral(path.node.id.name)]
+          )
+          if (lastExpression.type !== 'ReturnStatement') {
+            lastExpression.insertAfter(timeEndStatment)
+          } else {
+            lastExpression.insertBefore(timeEndStatment)
+          }
+        },
+      },
+    },
   }
 }
